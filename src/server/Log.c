3289a4cf115447d11d41ad22a2a9e6f6374d7766
Chremmer <barker8101@gmail.com>
2023-10-02 20:49:16 -0400

    Added functionality to Logger

--- src/server/Log.c
+++ src/server/Log.c
@@ -2,3 +2,65 @@
  * \addtogroup Log
 */
 #include "Log.h"
+
+LogSettings L;
+
+void InitializeLogger(FILE* _printStream, char outputLevel, char printToStdOut) {
+    L.ostream = _printStream;
+    L.outputLevel = outputLevel;
+    L.printToStdOut = printToStdOut;
+}
+
+void _logf(int level, const char * format, va_list argptr) {
+
+    // TODO add logic to include timestamp to the output
+    if(level >= L.outputLevel) {
+        vfprintf(L.ostream, format, argptr);
+    }
+
+    if(L.printToStdOut != 0) { //should only print if the output meets the required level as well?
+        vfprintf(stdout, format, argptr);
+    }
+}
+
+void LogfFatal(const char * format, ...) {
+    va_list argptr;
+    va_start(argptr, format);
+    _logf(FATAL, format, argptr); 
+    va_end(argptr); 
+}
+
+void LogfError(const char * format, ...) {
+    va_list argptr;
+    va_start(argptr, format);
+    _logf(ERROR, format, argptr);
+    va_end(argptr); 
+}
+
+void LogfWarning(const char * format, ...) {
+    va_list argptr;
+    va_start(argptr, format);
+    _logf(WARNING, format, argptr);
+    va_end(argptr); 
+}
+
+void LogfInfo(const char * format, ...) {
+    va_list argptr;
+    va_start(argptr, format);
+    _logf(INFO, format, argptr);
+    va_end(argptr); 
+}
+
+void LogfDebug(const char * format, ...) {
+    va_list argptr;
+    va_start(argptr, format);
+    _logf(DEBUG, format, argptr);
+    va_end(argptr); 
+}
+
+void LogfTrace(const char * format, ...) {
+    va_list argptr;
+    va_start(argptr, format);
+    _logf(TRACE, format, argptr);
+    va_end(argptr); 
+}
--- src/server/Log.h
+++ src/server/Log.h
@@ -1,5 +1,8 @@
 #ifndef Log_h
 #define Log_h
+
+#include <stdio.h>
+
 /**
  * \defgroup Log
  * \brief Handles logging; can be to the console or a file.
@@ -9,13 +12,86 @@
  * @todo Be able to set log levels
 */
 
+/**
+ * @brief These define the levels that a log can be printed ad
+ * 
+ */
+#define TRACE 0
+#define DEBUG 1
+#define INFO 2
+#define WARNING 3
+#define ERROR 4
+#define FATAL 5
+
+typedef struct 
+{
+    /// @brief output the logger should go to
+    FILE * ostream;
+    /// @brief minimum level that the log should be in order to ouput it
+    char outputLevel;
+    /// @brief if not 0 will print to stdout as well as ostream
+    char printToStdOut;
+
+} LogSettings;
+
+
+/**
+ * @brief Logs at a FATAL level the formatted string and parameters of the string
+ * 
+ * @param format formated string
+ * @param ... what to put in the formated string
+ */
+void LogfFatal(const char * format, ...);
+
+/**
+ * @brief Logs at an ERROR level the formatted string and parameters of the string
+ * 
+ * @param format formated string
+ * @param ... what to put in the formated string
+ */
 void LogfError(const char * format, ...);
 
-void LogfSuccess(const char * format, ...);
-
+/**
+ * @brief Logs at a WARNING level the formatted string and parameters of the string
+ * 
+ * @param format formated string
+ * @param ... what to put in the formated string
+ */
 void LogfWarning(const char * format, ...);
 
-void Log(const char * format, ...);
+/**
+ * @brief Logs at a INFO level the formatted string and parameters of the string
+ * 
+ * @param format formated string
+ * @param ... what to put in the formated string
+ */
+void LogfInfo(const char * format, ...);
+
+/**
+ * @brief Logs at a DEBUG level the formatted string and parameters of the string
+ * 
+ * @param format formated string
+ * @param ... what to put in the formated string
+ */
+void LogfDebug(const char* format, ...);
+
+/**
+ * @brief Logs at a TRACE level the formatted string and parameters of the string
+ * 
+ * @param format formated string
+ * @param ... what to put in the formated string
+ */
+void LogfTrace(const char * format, ...);
+
+/**
+ * @brief Instaiates the logger settings to run off of, must be called before logging can occur
+ * 
+ * @param _printStream output stream to print at
+ * @param outputLevel mimumum level needed to print to the printstream
+ * @param printToStdOut will print everyting to stdout if not set to 0 as well as _printStream
+ */
+void InitializeLogger(FILE* _printStream, char outputLevel, char printToStdOut);
+
 
 /**
  * @}
